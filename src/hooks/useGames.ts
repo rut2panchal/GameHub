fe44446ca124c2b53a21653233285ca1babe1326
Rutvik Panchal <rutvikpanchal89@gmail.com>
2024-08-29 10:01:21 -0400

    custom hook for fecth games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGameRes {
+    count: number;
+    results: Game[];
+}
+
+const Games = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+    useEffect(() => {
+        const controller = new AbortController();
+        apiClient
+            .get<FetchGameRes>("/games", { signal: controller.signal })
+            .then((res) => setGames(res.data.results))
+            .catch((error) => {
+                if (error instanceof CanceledError) return;
+                setError(error.message)
+            });
+
+        return () => controller.abort();
+    }, []);
+    return { games, error }
+}
+
+export default Games;
